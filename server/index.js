57d40c715f141d61edae376f4c2664d46f0a0441
Surya9122Prakash <sspsurya2002@gmail.com>
2023-12-18 21:39:36 +0530

    deploy commit

---
+++ server/index.js
@@ -0,0 +1,54 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+const dotenv=require('dotenv')
+const cors = require('cors')
+const path = require('path')
+const authRoute = require("./routes/auth.js")
+const userRoute = require("./routes/users.js")
+const postRoute = require("./routes/posts.js") 
+const commentRoute = require("./routes/comments.js")
+const cookieParser = require("cookie-parser")
+const multer = require("multer")
+
+const connectDB = async()=>{
+  try{
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log("Database Connected Successfully!!")
+  } catch(err){
+    console.log(err)
+  }
+}
+
+dotenv.config()
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({origin:'https://localhost:3000',credentials:true}))
+app.use("/api/auth",authRoute)
+app.use("/api/user",userRoute)
+app.use("/api/posts",postRoute)
+app.use("/api/comments",commentRoute)
+app.use("/images",express.static(path.join(__dirname,"/images")))
+
+const storage=multer.diskStorage({
+  destination:(req,file,fn)=>{
+      fn(null,"images")
+  },
+  filename:(req,file,fn)=>{
+      fn(null,req.body.img)
+      // fn(null,"image1.jpg")
+  }
+})
+
+const upload=multer({storage:storage})
+app.post("/api/upload",upload.single("file"),(req,res)=>{ 
+    res.status(200).json("Image has been uploaded successfully!")
+})
+
+app.listen(process.env.PORT,()=>{
+  connectDB();
+  console.log("App is Runnning on PORT "+process.env.PORT)
+})
+
+module.exports=app;
