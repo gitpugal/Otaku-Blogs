1fa517a7c2e7edb3dfd338f3f593c484d4598f2f
gitpugal <45727933+gitpugal@users.noreply.github.com>
2023-12-18 23:19:06 +0530

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -5,7 +5,15 @@ const bcrypt = require("bcrypt")
 const Post = require("../models/Post")
 const Comment = require("../models/Comment")
 const verifyToken = require("../verifyToken")
-
+const connectDB = async()=>{
+  try{
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log("Database Connected Successfully!!")
+  } catch(err){
+    console.log(err)
+  }
+}
+connectDB();
 router.post("/create",verifyToken,async (req,res)=>{
   try{
       const newPost=new Post(req.body)
